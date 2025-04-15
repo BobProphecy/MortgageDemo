20a493d7a5652c577e76848bbeb14654d0ce439a
bobw@prophecy.io <bobw@prophecy.io>
2025-04-15 13:36:59 +0000

    Update prophecy-libs dependencies to version 1.9.45 and 3.3.0-8.9.0 across jobs and pipelines, and remove appName from pipeline configurations.

--- pipelines/mortgage_analytics/code/mortgage_analytics/pipeline.py
+++ pipelines/mortgage_analytics/code/mortgage_analytics/pipeline.py
@@ -24,7 +24,6 @@ def main():
                 .config("spark.default.parallelism", "4")\
                 .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
                 .enableHiveSupport()\
-                .appName("Prophecy Pipeline")\
                 .getOrCreate()
     Utils.initializeFromArgs(spark, parse_args())
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/mortgage_analytics")
